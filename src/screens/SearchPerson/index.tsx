7d3963789ab6a26dff00998831fffc2d5143f123
Ana Clara Cabral Ramos <anaclaracabralramos@MacBook-Pro-de-Ana.local>
2026-01-13 19:50:29 -0300

    feat: searchPeople loading

--- src/screens/SearchPerson/index.tsx
+++ src/screens/SearchPerson/index.tsx
@@ -32,76 +32,77 @@ const SearchPerson = () => {
     </View>
   );
 
-  if (isLoading)
-    return (
-      <ActivityIndicator size="large" color="#101820" style={{ flex: 1 }} />
-    );
-
   return (
     <Wrapper title="Consultar indivíduo">
-      <View style={styles.inputContainer}>
-        <Ionicons
-          name="search"
-          size={20}
-          color="#1d4ed8"
-          style={styles.inputIcon}
-        />
+      {isLoading ? (
+        <ActivityIndicator size="large" color="white" style={{ flex: 1 }} />
+      ) : (
+        <>
+          <View style={styles.inputContainer}>
+            <Ionicons
+              name="search"
+              size={20}
+              color="#1d4ed8"
+              style={styles.inputIcon}
+            />
 
-        <TextInput
-          style={styles.input}
-          placeholder="Buscar por nome ou CPF..."
-          placeholderTextColor="#666"
-          value={searchQuery}
-          onChangeText={setSearchQuery}
-          autoCorrect={false}
-        />
+            <TextInput
+              style={styles.input}
+              placeholder="Buscar por nome ou CPF..."
+              placeholderTextColor="#666"
+              value={searchQuery}
+              onChangeText={setSearchQuery}
+              autoCorrect={false}
+            />
 
-        {/* Lógica: Se estiver carregando, mostra o spinner. 
+            {/* Lógica: Se estiver carregando, mostra o spinner. 
       Se não estiver e tiver texto, mostra o X para limpar */}
-        {isLoading ? (
-          <ActivityIndicator
-            size="small"
-            color="#1d4ed8"
-            style={{ marginRight: 10 }}
-          />
-        ) : (
-          searchQuery.length > 0 && (
-            <TouchableOpacity
-              onPress={() => setSearchQuery("")}
-              hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }} // Aumenta a área de toque
-            >
-              <Ionicons
-                name="close-circle"
-                size={20}
-                color="#666"
+            {isLoading ? (
+              <ActivityIndicator
+                size="small"
+                color="#1d4ed8"
                 style={{ marginRight: 10 }}
               />
-            </TouchableOpacity>
-          )
-        )}
-      </View>
+            ) : (
+              searchQuery.length > 0 && (
+                <TouchableOpacity
+                  onPress={() => setSearchQuery("")}
+                  hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }} // Aumenta a área de toque
+                >
+                  <Ionicons
+                    name="close-circle"
+                    size={20}
+                    color="#666"
+                    style={{ marginRight: 10 }}
+                  />
+                </TouchableOpacity>
+              )
+            )}
+          </View>
 
-      <FlatList
-        data={peopleList}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            style={styles.personCard}
-            onPress={() =>
-              navigation.navigate("PersonDetails", { id: item.id })
-            }
-          >
-            <View style={styles.personInfo}>
-              <Text style={styles.personName}>{item.nome}</Text>
-              <Text style={styles.personCpf}>CPF: {item.cpf}</Text>
-            </View>
-            <Ionicons name="chevron-forward" size={20} color="#334155" />
-          </TouchableOpacity>
-        )}
-        onRefresh={refetch}
-        refreshing={isLoading}
-        ListEmptyComponent={renderEmptyComponent}
-      />
+          <FlatList
+            data={peopleList}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={styles.personCard}
+                onPress={() =>
+                  navigation.navigate("PersonDetails", { id: item.id })
+                }
+              >
+                <View style={styles.personInfo}>
+                  <Text style={styles.personName}>{item.nome}</Text>
+                  <Text style={styles.personCpf}>CPF: {item.cpf}</Text>
+                </View>
+                <Ionicons name="chevron-forward" size={20} color="#334155" />
+              </TouchableOpacity>
+            )}
+            onRefresh={refetch}
+            refreshing={isLoading}
+            ListEmptyComponent={renderEmptyComponent}
+          />
+        </>
+      )}
     </Wrapper>
   );
 };
