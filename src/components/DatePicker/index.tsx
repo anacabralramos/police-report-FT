e1cf8c3f8a2652e1ed63df6a8c54f7546eb225a4
Ana Clara Cabral Ramos <anaclaracabralramos@MacBook-Pro-de-Ana.local>
2026-01-16 18:40:39 -0300

    feat: DatePicker adjustments

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -1,26 +1,24 @@
-import React, { useRef, useState } from "react";
-import { View, Text, TouchableOpacity, Platform } from "react-native";
-
+import React, { useState } from "react";
+import { Platform } from "react-native";
 import DateTimePicker, {
   DateTimePickerEvent,
 } from "@react-native-community/datetimepicker";
+
 import { Ionicons } from "@expo/vector-icons";
 
-import { styles } from "./styles";
+import Typography from "components/Typography";
+import Card from "components/Card";
 
 interface DatePickerProps {
   date: Date;
   onChange: (date: Date) => void;
-  label: string;
   mode?: "date" | "datetime";
 }
 export default function DatePicker({
   date,
   onChange,
-  label,
   mode = "date",
 }: DatePickerProps) {
-  const viewRef = useRef<View>(null);
   const [tempDate, setTempDate] = useState(date);
   const [show, setShow] = useState(false);
 
@@ -46,53 +44,46 @@ export default function DatePicker({
   };
 
   return (
-    <View style={styles.container} ref={viewRef}>
-      <Text style={styles.label}>{label}</Text>
+    <Card
+      onPress={() => setShow(true)}
+      disabled={Platform.OS === "ios"}
+      childrenCustomStyles={{
+        flexDirection: "row",
+        alignItems: "center",
+      }}
+    >
+      <Ionicons name="calendar" size={20} color="#1d4ed8" />
 
-      {/* O TouchableOpacity é necessário para o Android disparar o abrir */}
-      <TouchableOpacity
-        style={styles.pickerButton}
-        onPress={() => setShow(true)}
-        disabled={Platform.OS === "ios"} // No iOS o botão nativo já lida com o toque
-      >
-        <Ionicons
-          name="calendar"
-          size={20}
-          color="#1d4ed8"
-          style={styles.icon}
+      {Platform.OS === "ios" ? (
+        // NO IOS: Mantemos o componente exposto (seu visual atual)
+        <DateTimePicker
+          value={tempDate > new Date() ? new Date() : tempDate}
+          mode={mode}
+          display={"default"}
+          onChange={onDateChange}
+          textColor="#fff"
+          locale="pt-BR"
+          maximumDate={new Date()}
+          accentColor="#1d4ed8"
+          themeVariant="dark"
         />
-
-        {Platform.OS === "ios" ? (
-          // NO IOS: Mantemos o componente exposto (seu visual atual)
-          <DateTimePicker
-            value={tempDate > new Date() ? new Date() : tempDate}
-            mode={mode}
-            display={"default"}
-            onChange={onDateChange}
-            textColor="#fff"
-            locale="pt-BR"
-            maximumDate={new Date()}
-            accentColor="#1d4ed8"
-            themeVariant="dark"
-          />
-        ) : (
-          // NO ANDROID: Exibimos apenas o texto, e o Picker só aparece quando 'show' for true
-          <>
-            <Text style={styles.text}>
-              {tempDate.toLocaleDateString("pt-BR")}
-            </Text>
-            {show && (
-              <DateTimePicker
-                value={tempDate > new Date() ? new Date() : tempDate}
-                mode={mode}
-                display="default"
-                onChange={onDateChange}
-                maximumDate={new Date()}
-              />
-            )}
-          </>
-        )}
-      </TouchableOpacity>
-    </View>
+      ) : (
+        // NO ANDROID: Exibimos apenas o texto, e o Picker só aparece quando 'show' for true
+        <>
+          <Typography variant="default">
+            {tempDate.toLocaleDateString("pt-BR")}
+          </Typography>
+          {show && (
+            <DateTimePicker
+              value={tempDate > new Date() ? new Date() : tempDate}
+              mode={mode}
+              display="default"
+              onChange={onDateChange}
+              maximumDate={new Date()}
+            />
+          )}
+        </>
+      )}
+    </Card>
   );
 }
