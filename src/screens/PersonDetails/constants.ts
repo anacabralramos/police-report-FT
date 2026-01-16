b85f8970fb8f1ca914406da941c0a6d67949878c
Ana Clara Cabral Ramos <anaclaracabralramos@MacBook-Pro-de-Ana.local>
2026-01-16 18:43:56 -0300

    feat: PersonDetails screen adjustments

---
+++ src/screens/PersonDetails/constants.ts
@@ -0,0 +1,17 @@
+export const INFO_FIELDS = [
+  {
+    label: "CPF",
+    value: "cpf",
+    icon: "document-text-outline",
+  },
+  {
+    label: "RG",
+    value: "rg",
+    icon: "id-card-outline",
+  },
+  {
+    label: "Nascimento",
+    value: "data_nascimento",
+    icon: "calendar-outline",
+  },
+];
--- src/screens/PersonDetails/index.tsx
+++ src/screens/PersonDetails/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { View, Text, ScrollView } from "react-native";
-import { RouteProp, useRoute } from "@react-navigation/native";
-import { Ionicons } from "@expo/vector-icons";
+import { View, ScrollView } from "react-native";
 
+import { Badge, Card, ImageCarousel, InfoField, Typography } from "@components";
+import { RouteProp, useRoute } from "@react-navigation/native";
 import { RootStackParamList } from "@navigation";
 import { usePersonDetails } from "@hooks";
 import { formatDate } from "@utils";
+
+import { INFO_FIELDS } from "./constants";
 import { styles } from "./styles";
 
 type PersonDetailsRouteProp = RouteProp<RootStackParamList, "PersonDetails">;
@@ -14,58 +16,46 @@ const PersonDetails = () => {
   const { params } = useRoute<PersonDetailsRouteProp>();
   const { data: personDetails } = usePersonDetails(params.id);
 
-  const InfoField = ({
-    label,
-    value,
-    icon,
-  }: {
-    label: string;
-    value: string;
-    icon: string;
-  }) => (
-    <View style={styles.infoBox}>
-      <View style={styles.iconContainer}>
-        <Ionicons name={icon as any} size={20} color="#1d4ed8" />
-      </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.label}>{label}</Text>
-        <Text style={styles.valueText}>{value || "Não informado"}</Text>
-      </View>
-    </View>
-  );
-
   return (
-    <ScrollView
-      style={{ flex: 1, backgroundColor: "#0A0F14" }}
-      contentContainerStyle={{ paddingBottom: 40 }}
-    >
-      {/* Cabeçalho de Identificação */}
-      <View style={styles.headerArea}>
-        <View style={styles.avatarCircle}>
-          <Ionicons name="person" size={50} color="#334155" />
-        </View>
-        <Text style={styles.mainName}>{personDetails.nome}</Text>
-        <Text style={styles.subTitle}>Consulta de Indivíduo</Text>
-      </View>
+    <ScrollView style={styles.wrapper}>
+      <ImageCarousel images={personDetails?.fotos} />
 
       <View style={styles.container}>
-        <InfoField
-          label="Nome Completo"
-          value={personDetails.nome}
-          icon="person-outline"
-        />
-        <InfoField label="CPF" value={personDetails.cpf} icon="card-outline" />
-        <InfoField label="RG" value={personDetails.rg} icon="id-card-outline" />
-        <InfoField
-          label="Data de Nascimento"
-          value={formatDate(personDetails.data_nascimento)}
-          icon="calendar-outline"
-        />
-        <InfoField
-          label="Endereço Residencial"
-          value={personDetails.endereco}
-          icon="location-outline"
-        />
+        <Typography>{personDetails?.nome || "Não informado"}</Typography>
+        <Badge text="INDIVÍDUO CADASTRADO" />
+        <View style={styles.contentContainer}>
+          <Card childrenCustomStyles={styles.card}>
+            <Typography variant="smallDefault">DOCUMENTAÇÃO</Typography>
+            <View style={styles.gridRow}>
+              {INFO_FIELDS.map((item, key) => {
+                if (!personDetails[item.value]) return;
+
+                const value =
+                  item.value === "data_nascimento"
+                    ? formatDate(personDetails[item.value] || "")
+                    : personDetails[item.value];
+
+                return (
+                  <InfoField
+                    key={key}
+                    label={item.label}
+                    value={value}
+                    icon={item.icon}
+                  />
+                );
+              })}
+            </View>
+          </Card>
+          <Card childrenCustomStyles={styles.card}>
+            <Typography variant="smallDefault">ENDEREÇO</Typography>
+
+            <InfoField
+              label="Residência"
+              value={personDetails?.endereco}
+              icon="location-outline"
+            />
+          </Card>
+        </View>
       </View>
     </ScrollView>
   );
